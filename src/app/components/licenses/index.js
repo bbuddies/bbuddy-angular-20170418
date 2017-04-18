a772aaf3bbfa200056bd3dee1c206479362c6e99
richardkuo <richardkuo@kkstream.com.tw>
2017-04-18 17:14:35 +0800

    Fix typo

--- src/app/components/licenses/index.js
+++ src/app/components/licenses/index.js
@@ -3,5 +3,5 @@ import list from './list.component'
 import add from './add.component'
 
 export default angular
-    .module('accounts', [list, add])
+    .module('licenses', [list, add])
     .name
