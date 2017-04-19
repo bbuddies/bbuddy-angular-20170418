37e8b7b973e028c188ccba089f9e3091f52abddb
richardkuo <richardkuo@kkstream.com.tw>
2017-04-19 10:55:42 +0800

    Add month restriction

--- src/app/components/licenses/licenses.model.js
+++ src/app/components/licenses/licenses.model.js
@@ -9,13 +9,32 @@ export default class Licenses{
         this.api.licenses.all(callback)
     }
     add(license, success, failure){
-        var tempMonth = license.month;
-        var tempAmount = license.amount.toString();
+        var tempMonth = license.month
+        var tempAmount = license.amount.toString()
+        var year = parseInt(tempMonth.split('-')[0]);
+        var month = parseInt(tempMonth.split('-')[1]);
 
-        if (tempMonth.trim().length == 0 || tempAmount.trim().length == 0){
-            failure('License month or amount should not be empty!')
+
+        if (tempMonth.trim().length == 0){
+            failure('License month should not be empty!')
             return
         }
+
+        if (tempAmount.trim().length == 0){
+            failure('License amount should not be empty!')
+            return
+        }
+
+        if (license.amount <= 0){
+            failure('License amount should be greater than zero!')
+            return
+        }
+
+        if (!year || !month || month > 12) {
+            failure('Please fill correct date EX:2017-02')
+            return
+        }
+
         this.api.licenses.add(license, success)
     }
 }
--- src/app/components/licenses/list.controller.js
+++ src/app/components/licenses/list.controller.js
@@ -3,7 +3,7 @@ import {Inject} from '../../common/decorators'
 @Inject('licensesModel')
 export default class LicensesController{
     constructor(licenses) {
-        licenses.fetchAll((data) => this.licenses = data)
+        licenses.fetchAll((data) => this.licenses = data.data)
     }
 }
 
