1bc5c0a34c6c58b9358a4542622c07d8a4d33555
EddieHsu <eddiehsu@kkstream.com.tw>
2017-04-19 10:39:43 +0800

    move logic to modal

--- src/app/components/licenses/add.controller.js
+++ src/app/components/licenses/add.controller.js
@@ -12,26 +12,9 @@ export default class LicensesAddController {
         this.message = ""
     }
     save(){
-        var flag = true
 
-        if(this.license.amount <= 0) {
-            this.message += 'amount must > 0. '
-            flag = false
-        } else {
-            this.message = ''
-        }
-
-        if(!/^\d\d\d\d([- /.])(0[1-9]|1[012])$/.test(this.license.month)) {
-            this.message += 'invalid month format, must be "yyyy-mm". '
-            flag = false
-        } else {
-            this.message = ''
-        }
-
-        if(flag) {
-            this.licenses.add(this.license,
-                () => this.$state.go('app.licenses.add'),
-                (message) => this.message = message)
-        }
+        this.licenses.add(this.license,
+            () => this.$state.go('app.licenses.add'),
+            (message) => this.message = message)
     }
 }
--- src/app/components/licenses/licenses.model.js
+++ src/app/components/licenses/licenses.model.js
@@ -6,8 +6,20 @@ export default class Licenses {
         this.api = api
     }
     add(license, success, failure){
-        console.log('2')
-        console.log(this.api)
-        this.api.licenses.add(license, success)
+        var flag = true
+
+        if(license.amount <= 0) {
+            failure('amount must > 0. ')
+            flag = false
+        }
+
+        if(!/^\d\d\d\d([- /.])(0[1-9]|1[012])$/.test(license.month)) {
+            failure('invalid month format, must be "yyyy-mm". ')
+            flag = false
+        }
+
+        if(flag) {
+            this.api.licenses.add(license, success)
+        }
     }
 }
