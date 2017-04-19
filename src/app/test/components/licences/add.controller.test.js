b9c8b6d5507fd4d792748b9a4bc85c50858e9d56
Gary Yeap <contact@garyyeap.com>
2017-04-19 09:33:57 +0800

    Unifies the license, licence variables

--- src/app/test/components/licences/add.controller.test.js
+++ src/app/test/components/licences/add.controller.test.js
@@ -1,13 +1,13 @@
 import Controller from '../../../components/licenses/add.controller';
 
-describe('licences add controller', function() {
-    var licences, $state, add, go, controller
+describe('licenses add controller', function() {
+    var licenses, $state, add, go, controller
     beforeEach(() => {
-        licences = {add: () => {}}
-        add = sinon.stub(licences, 'add').yields()
+        licenses = {add: () => {}}
+        add = sinon.stub(licenses, 'add').yields()
         $state = {go: ()=>{}}
         go = sinon.spy($state, 'go')
-        controller = new Controller(licences, $state)
+        controller = new Controller(licenses, $state)
         controller.license.month = '2017-02'
         controller.license.amount = 500
     })
--- src/app/test/components/licences/licences.model.test.js
+++ src/app/test/components/licences/licences.model.test.js
@@ -1,17 +1,17 @@
 import Licenses from '../../../components/licenses/licenses.model';
 
-describe('licences model', function() {
-    var licence = {month: '2017-02', amount: 500}
-    var api, add, licences, success, failure
+describe('licenses model', function() {
+    var license = {month: '2017-02', amount: 500}
+    var api, add, licenses, success, failure
     beforeEach(() => {
         api = {licenses: {add: () => {}}}
         add = sinon.stub(api.licenses, 'add').yields({success: true, errors: []})
-        licences = new Licenses(api)
+        licenses = new Licenses(api)
         success = sinon.spy()
         failure = sinon.spy()
     })
-    it('add an licence successfully', function(){
-        licences.add(licence, success, failure)
+    it('add an license successfully', function(){
+        licenses.add(license, success, failure)
 
         add.should.have.been.calledWith({month: '2017-02', amount: 500})
         success.should.have.been.called
