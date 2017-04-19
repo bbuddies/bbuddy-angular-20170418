c7663ed625c376b7916f9f34aa14ad8347162df9
Liz Lin <liztmlin@kkstream.com.tw>
2017-04-19 10:18:11 +0800

    add 0 amount error test

--- src/app/test/components/licenses/add.controller.test.js
+++ src/app/test/components/licenses/add.controller.test.js
@@ -1,12 +1,14 @@
 import Controller from '../../../components/licenses/add.controller';
 
 describe('licenses add controller', function() {
-    var license, $state, add, go, controller
+    var license, $state, add, alert, go, controller
     beforeEach(() => {
         license = {add: () => {}}
         add = sinon.stub(license, 'add').yields()
         $state = {go: ()=>{}}
         go = sinon.spy($state, 'go')
+        global.window = {alert: () => {}}
+        alert = sinon.stub(window, "alert", function(msg) { return msg; } );
         controller = new Controller(license, $state)
         controller.license.month = '2017-03'
         controller.license.amount= 100000
@@ -16,6 +18,12 @@ describe('licenses add controller', function() {
 
         add.should.have.been.calledWith({month: '2017-03', amount: 100000})
         go.should.have.been.calledWith('app.licenses')
+    })
+    it('set amount is 0 is invalid', function(){
+        controller.license.amount = 0;
+        controller.save();
+
+        alert.should.have.been.called;
     })
 
     // it('add an account failed', function(){
