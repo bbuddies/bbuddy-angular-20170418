48543e7f22192c63e65e5db44245c3dbfc3aa6a9
Gary Yeap <contact@garyyeap.com>
2017-04-19 18:23:21 +0800

    Fix tests

--- src/app/test/components/licenses/sum.model.test.js
+++ src/app/test/components/licenses/sum.model.test.js
@@ -16,7 +16,7 @@ describe('licenses model', function() {
 
       var result = licensesModel._sumInLocal(licenses, '2017-01-03', '2017-01-30')
 
-      result.should.be.eql(180.65)
+      result.should.be.eql('180.65')
     })
 
     it('Verify cross months license period', function(){
@@ -33,27 +33,26 @@ describe('licenses model', function() {
 
       var result = licensesModel._sumInLocal(licenses, '2017-01-03', '2017-05-30')
 
-      result.should.be.eql(187.1)
+      result.should.be.eql('187.10')
     })
 
-    it('Verify cross months license period', function(){
+    it('Verify cross years license period', function(){
       var licenses = [
         {
-          month: '2017-01',
-          amount: 200
+          month: '2017-12',
+          amount: 1000
         },
         {
-          month: '2017-06',
-          amount: 500
+          month: '2018-01',
+          amount: 1000
         }
       ];
 
-      var result = licensesModel._sumInLocal(licenses, '2016-12-01', '2017-07-30')
-
-      result.should.be.eql(700.00.toString())
+      var result = licensesModel._sumInLocal(licenses, '2017-12-12', '2018-01-10')
+      result.should.be.eql('967.74')
     })
 
-    it('Verify cross months license period', function(){
+    it('Verify cross months with middle date license period', function(){
       var licenses = [
         {
           month: '2017-02',
@@ -67,6 +66,6 @@ describe('licenses model', function() {
 
       var result = licensesModel._sumInLocal(licenses, '2017-02-15', '2017-03-30')
 
-      result.should.be.eql(583.87)
+      result.should.be.eql('583.87')
     })
 })
