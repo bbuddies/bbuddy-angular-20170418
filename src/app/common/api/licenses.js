d319339c41d9d698ad9e0002689cce5b87123a8d
EddieHsu <eddiehsu@kkstream.com.tw>
2017-04-19 10:13:59 +0800

    add month and amount check

--- src/app/common/api/licenses.js
+++ src/app/common/api/licenses.js
@@ -5,8 +5,8 @@ export default class Licenses {
     constructor(http){
         this.http = http
     }
-    add(license, callback){
-        console.log('yo')
+    add(license, callback) {
+        //console.log('yo')
         this.http.post("licenses", license, callback)
     }
 }
--- src/app/components/licenses/add.controller.js
+++ src/app/components/licenses/add.controller.js
@@ -12,7 +12,19 @@ export default class LicensesAddController {
         this.message = ""
     }
     save(){
-        console.log('1')
+        //console.log('1')
+        if(this.license.amount <= 0) {
+            this.message += 'amount must > 0. '
+        } else {
+            this.message = ''
+        }
+
+        if(!/^\d\d\d\d([- /.])(0[1-9]|1[012])$/.test(this.license.month)) {
+            this.message += 'invalid month format, must be "yyyy-mm". '
+        } else {
+            this.message = ''
+        }
+
         this.licenses.add(this.license,
             () => this.$state.go('app.licenses.add'),
             (message) => this.message = message )
